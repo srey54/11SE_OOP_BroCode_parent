f6042d9b9ab96a0e3fddcff28ed9d04f620cd256
Sreyansh <sveldandi@students.rj.nsw.edu.au>
2026-05-01 11:14:34 +1000

    Initial commit: Car class OOP example

---
+++ parent.py
@@ -0,0 +1,19 @@
+Class Parent:
+
+       def __init__(self, first_name, favorite_food, hobby):
+                self.first_name = first_name
+                self.favourite_food = favorite_food
+                self.hobby = hobby
+
+        def introduce(self):
+            print("Srey likes donuts and bowling")
+        def relax(self):
+                print("Srey is now relaxed")
+        def indulge(self):
+                print("Srey is now eating a ")
+
+
+
+
+
+
